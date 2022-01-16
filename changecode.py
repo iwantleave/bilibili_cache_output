d3b88367b8f90895015b47abcce97cff6f1c3cf4
iwantleave <wutianhong@gmail.com>
2022-01-16 18:10:48 +0800

    加了大量报错处理，修改各种处理逻辑，自动处理文件名

--- changecode.py
+++ changecode.py
@@ -11,14 +11,6 @@ def getVideoFilename():
     videoFilename.set(filepath)  # 设置变量filename的值
     print("videoFilename=",filepath)
 
-
-def getAudioFilename():
-    filepath = askopenfilename()  # 选择目录，返回目录名
-    audioFilename.set(filepath)  # 设置变量outputpath的值
-    print("audioFilename=",filepath)
-
-
-
 def fileSave():
     outputFilePath = askdirectory()  # 选择目录，返回目录名
     savepath.set(outputFilePath)  # 设置变量outputpath的值
@@ -43,75 +35,47 @@ def mergeFile():
 
 def mergeFile1():
     print("开始简单合并")
-    print("tmppath=",tmppath.get())
+    print(tmppath.get())
 
-
+    videoFilename1=tmppath.get()+"/1/80/video.m4s"
+    audioFilename1 = tmppath.get()+"/1/80/audio.m4s"
     savepath1 = "D:/bilibili/"
     jsonPath=tmppath.get()+"/1/entry.json"
     print("jsonPath=",jsonPath)
-    entryJson = json.load(open(jsonPath,'r',encoding='utf-8'))
-    print("entryJson=", entryJson)
-    title = entryJson['title'].strip().replace(" ",'_')
-    type_tag = entryJson['type_tag'].strip()
-    bvid = entryJson['bvid'].strip()
-    owner_id = str(entryJson['owner_id'])
-    print("type_tag=", type_tag, "bvid=", bvid, "owner_id=", owner_id);
+    tmpJson = json.load(open(jsonPath,'r',encoding='utf-8'))
+    print("tmpJson=", tmpJson)
+    title = tmpJson['title'].strip().replace(" ",'_')
 
-    # 获取文件路径
-    videoFilename1 = tmppath.get()+"/1/" + type_tag +"/video.m4s"
-    audioFilename1 = tmppath.get()+"/1/" + type_tag +"/audio.m4s"
-
-
-    # 处理不合适的字符
-    rstr = r"[\/\\\:\*\?\"\<\>\|&]"  # '/ \ : * ? " < > | &'
+    rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
     title = re.sub(rstr, "_", title)
 
     print("title",title)
 
-    # 输出文件格式 文件名_bvid_ownerid.mp4
-    outputFileName = savepath1 + title + "_" + bvid + "_" + owner_id + ".mp4"
-
     # 检查参数
     errormessage = "";
     if len(videoFilename1) == 0:
-        print("错误，视频为空！")
-        showwarning("错误", "视频为空")
+        showinfo("错误", "视频为空")
         return
     if not os.path.isfile(videoFilename1):
-        print("错误，视频文件不存在！")
-        showwarning("错误", "视频文件不存在")
+        showinfo("错误", "视频文件不存在")
         return
     if len(audioFilename1) == 0:
-        print("错误，音频为空！")
-        showwarning("错误", "音频为空")
+        showinfo("错误", "音频为空")
         return
     if not os.path.isfile(audioFilename1):
-        print("错误，音频文件不存在！")
-        showwarning("错误", "音频文件不存在")
+        showinfo("错误", "音频文件不存在")
         return
 
     if len(savepath1) == 0:
-        print("错误，保存路径为空！")
-        showwarning("错误", "保存路径为空")
+        showinfo("错误", "保存为空")
         return
     if not os.path.exists(savepath1):
-        print("错误，保存路径不存在！")
-        showwarning("错误", "保存路径不存在")
-        return
-    if os.path.isfile(outputFileName):
-        print("错误，输出视频文件已存在！")
-        showwarning("错误", "输出视频文件已存在")
+        showinfo("错误", "保存路径不存在")
         return
 
-    cmd = "ffmpeg.exe -report -i " + videoFilename1 + " -i " + audioFilename1 + " -c copy " + outputFileName
+    cmd = "ffmpeg.exe -report -i " + videoFilename1 + " -i " + audioFilename1 + " -c copy " + savepath1 + title + ".mp4"
     print("cmd=",cmd)
-    if os.system(cmd) == 0:
-        print("合并完成！")
-        showinfo(title="成功", message="合并完成！"+outputFileName)
-    else:
-        print("合并失败！")
-        showerror(title="失败", message="合并失败！")
-
+    os.system(cmd)
 
 
 if __name__ == '__main__':
@@ -134,10 +98,6 @@ if __name__ == '__main__':
     tk.Label(root, text='选择视频文件').grid(row=3, column=0, padx=5, pady=5)
     tk.Entry(root, textvariable=videoFilename).grid(row=3, column=1, padx=5, pady=5)
     tk.Button(root, text='打开', command=getVideoFilename).grid(row=3, column=2, padx=5, pady=5)
-
-    tk.Label(root, text='选择音频文件').grid(row=4, column=0, padx=5, pady=5)
-    tk.Entry(root, textvariable=audioFilename).grid(row=4, column=1, padx=5, pady=5)
-    tk.Button(root, text='打开', command=getAudioFilename).grid(row=4, column=2, padx=5, pady=5)
 
     # 构建“保存文件”这一行的标签、输入框以及启动按钮
     tk.Label(root, text='选择保存目录').grid(row=5, column=0, padx=5, pady=5)
